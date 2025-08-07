68f76ae674b319f003e88a55f197132617110a8a
Truong Ngoc Gia Hieu (TOM) <giahieutruongngoc@gmail.com>
2025-08-07 10:17:30 +0700

    Add files via upload

---
+++ 1.1P/Code/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Task_1_1_P
+{
+    public class Mainprogram
+    {
+        public static void Average()
+        {
+            Console.WriteLine("Please enter the array of integers: "); //Ask user for input the arrat of integers
+            string input_numbers = Console.ReadLine(); //Read the input from the user
+            string[] numbers = input_numbers.Split(' '); //Split the input string into an array of strings
+            int[] number_list = new int[numbers.Length]; //Create an array of integers with the same length as the input string array
+            Console.WriteLine("-----------------");
+            Console.WriteLine("The arrays of integers you entered: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                number_list[i] = Convert.ToInt32(numbers[i]);
+                sum += number_list[i];
+            }
+            int average = sum/numbers.Length;
+            Console.WriteLine($"The average of the list is: {average}");
+
+        }
+        public static void Main(string[] args)
+        {
+            Average();
+            Console.ReadKey(); //Wait for user to press a key before closing the console window
+        }
+    }
+}
---
+++ 1.2P/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions; // Import for Regex
+
+namespace _1._2P
+{
+    internal class Greetingprogram
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Task_1_2_P"; // Set the window title
+            Console.Write("Dear user, please enter your name: ");
+            string input = Console.ReadLine().Trim(); // Read input and trim whitespace
+
+            if (input == "exit" || input == "Exit") //check for exit
+            {
+                Console.WriteLine("Goodbye!");
+                return; // Exit the program
+            }
+
+            // Check if the input is null, empty, or contains special characters
+            if (string.IsNullOrEmpty(input) || Regex.IsMatch(input, "[^a-zA-Z\\s]"))
+            {
+                Console.WriteLine("Welcome, nice to meet you!");
+            }
+            else
+            {
+                string[] user_name = input.Split(' '); // Split the input by spaces.
+
+                if (user_name.Length >= 3)
+                {
+                    for (int i = 0; i < user_name.Length; i++)
+                    {
+                        Console.WriteLine($"Hi {user_name[i]}, how are you?");
+                    }
+                }
+                else if (user_name.Length == 1)
+                {
+                    Console.WriteLine("Welcome Admin");
+                }
+            }
+
+            Console.ReadKey(); // Wait for user to press any key before exiting
+        }
+    }
+}
