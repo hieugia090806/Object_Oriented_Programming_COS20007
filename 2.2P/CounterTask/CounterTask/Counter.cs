40f1e6cf60a03d91adf8352a3ac4cdbe7f16f0df
Truong Ngoc Gia Hieu (TOM) <giahieutruongngoc@gmail.com>
2025-08-07 10:19:02 +0700

    Add files via upload

---
+++ 2.2P/CounterTask/CounterTask/Counter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    public class Counter
+    {
+        private double _count;
+        private string _name;
+        
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public Counter(string name)
+        {
+            _name = name;
+            _count = 0;
+        }
+        public void Increment()
+        {
+            _count++;
+        }
+        
+        public void Reset()
+        {
+            _count = 0;
+        }
+        public double Ticks
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public void ResetByDefault()
+        {
+            _count = 214783647520;
+        }
+    }
+}
---
+++ 2.2P/CounterTask/CounterTask/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Clock
+{
+    internal class Program
+    {
+        private static void PrintCounter(Counter counter)
+        {
+            Console.WriteLine($"Counter Name: {counter.Name}, Ticks: {counter.Ticks}");
+        }
+        static void Main(string[] args)
+        {
+            Counter[] myCounters = new Counter[3];
+            myCounters[0] = new Counter("Counter 1");
+            myCounters[1] = new Counter("Counter 2");
+            myCounters[2] = myCounters[0];
+
+            for (int i = 0; i <= 9; i++)
+            {
+                myCounters[0].Increment();
+            }
+
+            for (int i = 1; i<= 14; i++)
+            {
+                myCounters[1].Increment();
+            }
+
+            Console.WriteLine("Print counters before reseting myCounters[2]");
+            PrintCounter(myCounters[0]);
+            PrintCounter(myCounters[1]);
+            PrintCounter(myCounters[2]);
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Print Counters after reseting myCounter[2]");
+            myCounters[2].Reset();
+            PrintCounter(myCounters[0]);
+            PrintCounter(myCounters[1]);
+            PrintCounter(myCounters[2]);
+        }
+    }
+}
