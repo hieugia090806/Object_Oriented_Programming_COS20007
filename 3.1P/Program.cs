2db2e8680bc4dfe698888ab0882e399f3ca8d827
Truong Ngoc Gia Hieu (TOM) <giahieutruongngoc@gmail.com>
2025-08-07 10:41:05 +0700

    Add files via upload

---
+++ 3.1P/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            
+            for (int j = 0; j < 86700; j++)
+            { 
+                Thread.Sleep(6000); ///The bigger number is, the slower the clock will tick.
+                Console.Clear();
+                clock.Tick();
+                Console.WriteLine(clock.Time());
+            }
+        }
+    }
+}
